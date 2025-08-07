ceddadc5f0a9c6286ab334af6464e8a9bbd618c0
Laalenthika <k.laalenthika@gmail.com>
2025-08-07 23:10:24 +0530

    added multiple carousels

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,6 +7,11 @@ import { Card, CardContent } from "@/components/ui/card";
 import Autoplay from "embla-carousel-autoplay"
 import { BookCarousel } from "@/components/BookCarousel";
 
+interface GenreCarousel {
+  genre: string;
+  books: Book[];
+};
+
 export default async function Home() {
     const supabase = await createClient();
     const { data: { user } } = await supabase.auth.getUser();
@@ -23,28 +28,48 @@ export default async function Home() {
     }
     
     let books: Book[] = [];
-    let genres = ["fiction", "fantasy", "non-fiction"];
+    // let genres = ["Fiction", "Fantasy", "Non-Fiction"];
+    let genres = ["Fiction", "Fantasy", "NonFiction"];
+    let genreCarousels: GenreCarousel[] = [];
 
     try{
       const apiKey = process.env.NEXT_PUBLIC_CLOUD_API;
-      const apiURL = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=12&key=${apiKey}`;
-      const response = await fetch(apiURL, { next: { revalidate: 3600 } });
+      // const apiURL = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=12&key=${apiKey}`;
+      // const response = await fetch(apiURL, { next: { revalidate: 3600 } });
 
-      if(!response.ok){
-        // toast.error(response.statusText); 
-        console.log(`Failed to fetch books: ${response.statusText}`);
+      const fetchPromises = genres.map(genre => {
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre.toLowerCase()}&maxResults=12&key=${apiKey}`;
+        return fetch(apiUrl, { next: { revalidate: 3600 } }).then(res => res.json());
       }
-      console.log("Success")
-      const responseData: GoogleBooksApiResponse = await response.json();
-      books = responseData.items || [];
+      )
+
+      const results: GoogleBooksApiResponse[] = await Promise.all(fetchPromises);
+      genreCarousels = results.map((res, index) => ({
+        genre: genres[index],
+        books: res.items?.filter(book => book.volumeInfo.imageLinks?.thumbnail) || [],
+      }));
+
+      // if(!response.ok){
+      //   // toast.error(response.statusText); 
+      //   console.log(`Failed to fetch books: ${response.statusText}`);
+      // }
+      // console.log("Success")
+      // const responseData: GoogleBooksApiResponse = await response.json();
+      // books = responseData.items || [];
 
     }catch(error){
       console.error("Failed Google books fetch , ", error);
     }
     return (
         <div className="flex flex-col">
-          <h1 className="text-3xl font-bold mb-6 text-center">Featured Fiction</h1>
-          <BookCarousel books={books}/>
+          {
+            genreCarousels.map(({genre, books}) => (
+              <div key={genre}>
+                <h2 className="text-3xl font-bold mb-6">{`Featured ${genre}`}</h2>
+                <BookCarousel books={books} />
+              </div>
+            )) 
+          }
         </div>
     );
 }
